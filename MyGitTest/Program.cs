fc81383d990e633f7bd0a68e4fd6f8d395b7814b
郭立明 <wayfat@outlook.com>
2020-01-20 16:19:15 +0800

    123

--- MyGitTest/Program.cs
+++ MyGitTest/Program.cs
@@ -14,6 +14,7 @@ namespace MyGitTest
         [STAThread]
         static void Main()
         {
+            //测试
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
